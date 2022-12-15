d2b8b6fc53883b727d2e82fb8113cf8de468b50b
marikabaah <baahmarika@gmail.com>
2022-12-15 17:59:27 -0600

    Second Push

--- marikaPage.ts
+++ marikaPage.ts
@@ -3,9 +3,21 @@ import { By } from 'selenium-webdriver'
 
 export class Marika extends BasePage {
     bookThisRoom: By = By.xpath ('//button[text()="Book this room"]')
-    hackBtn: By = By.xpath('//button[text()= "Let ne hack!"]')
+    hackBtn: By = By.xpath('//button[text()= "Let me hack!"]')
     bookBtn: By = By.xpath('//button[text()= "Book"]')
-    cancelBtn: By = By.xpath('//button[text()= "Cancel"')
+    bookName: By = By.xpath('//input[@name="firstname"]')
+    bookLastName: By = By.xpath('//input[@name="lastname"]')
+    bookEmail: By = By.xpath('//input[@name="email"]')
+    bookPhone: By = By.xpath('//input[@name="phone"]')
+    cancelBtn: By = By.xpath('//button[text()= "Cancel"]')
+    //Test2
+    name: By = By.id('name')
+    email: By = By.id('email')
+    phoneNum: By = By.id('phone')
+    subject: By = By.id('subject')
+    msg: By = By.id('description')
+    submit: By = By.id('submitContact')
+
     //selectors - what we are locating/ manipulatiing 
     constructor() {
         super({url: "https://automationintesting.online/"})
@@ -16,39 +28,3 @@ export class Marika extends BasePage {
     }
 }
 //selectors for contacting the hotel 
-import {Builder, Capabilities, WebDriver, until, WebElement} from "selenium-webdriver"; 
-export class RestPage {
-    driver: WebDriver;
-    url: string = "https://automationintesting.online";
-    name: By = By.id('name')
-    email: By = By.id('email')
-    phoneNum: By = By.id('phone')
-    subject: By = By.id('subject')
-    msg: By = By.id('description')
-    submit: By = By.id('submitContact')
-    error: By = By.className('alert alert-danger')
-    constructor(driver: WebDriver) {
-        this.driver = driver;
-    }
-    //methods 
-    async navigate(url:string): Promise<void>{
-        if (url) return await this.driver.get(url);
-        else if (this.url) return await this.driver.get(this.url)
-        else return Promise.reject(
-            "BasePage.navigate() needs a url defined on the page objects"
-        )
-    }
-    async sendKeys(elementBy: By, Keys){
-        await this.driver.wait(until.elementsLocated(elementBy))
-        return this.driver.findElement(elementBy).sendKeys(Keys)
-    }
-    async getElemet(elementBy: By): Promise<WebElement>{
-        await this.driver.wait(until.elementLocated(elementBy));
-        let element = await this.driver.findElement(elementBy);
-        await this.driver.wait(until.elementIsVisible(element))
-        return element; 
-    }
-    async Click(elementBy: By): Promise<void> {
-        return(await this.getElemet(elementBy)).click();
-    }
-}
--- marikaTest.test.ts
+++ marikaTest.test.ts
@@ -1,26 +1,47 @@
+import { By } from "selenium-webdriver";
 import { Marika } from "./marikaPage";
 const marika = new Marika ()
 
 describe('Marika Solo Capstone Project', () => {
     test('opening url and clicking on book', async () => {
-        await marika.navigate()
-        await marika.click(marika.hackBtn)
-        await marika.clickToAccessBookingCal
-        await marika.click(marika.bookBtn)
+        await marika.navigate();
+        await marika.driver.sleep(5000)
+        await marika.click(marika.hackBtn);
+        await marika.clickToAccessBookingCal();
+        await marika.click(marika.bookName);
+        await marika.setInput(marika.bookName, "Jane");
+        await marika.click(marika.bookLastName);
+        await marika.setInput(marika.bookLastName, "Doe")
+        await marika.click(marika.email);
+        await marika.setInput(marika.email, "janedoe@yahoo.com");
+        await marika.click(marika.bookPhone);
+        await marika.setInput(marika.bookPhone, "7184710988");
+        await marika.click(marika.bookBtn);
         await marika.click(marika.cancelBtn)
-        await marika.driver.quit()
+    
     })
-})
+    test('contact us', async () => {
+        await marika.navigate();
+        await marika.click(marika.name);
+        await marika.setInput(marika.name, "Jane Doe");
+        await marika.click(marika.email);
+        await marika.setInput(marika.email, "janedoe@yahoo.com");
+        await marika.click(marika.phoneNum);
+        await marika.setInput(marika.phoneNum, "7184710988");
+        await marika.click(marika.subject);
+        await marika.setInput(marika.subject, "My July Booking");
+        await marika.click(marika.msg);
+        await marika.setInput(marika.msg, "Please confirm my July booking");
+        await marika.click(marika.submit);
+        await marika.driver.quit()
+        
 
-import { RestPage } from "./marikaPage";
-const chromedriver = require("chromedriver");
-test('contact us', async () => {
-    await marika.navigate();
-    await marika.getElement(page.name);
-    await marika.setInput(page.name, "Jane Doe");
-    await marika.getElement(page.email);
-    await marika.setInput(page.email, "janedoe@yahoo.com");
-    await marika.getElement(page.phoneNum);
-    await marika.setInput(page.phoneNum, "7188682828");
+    
+    
+    
+    })
     
 })
+
+
+
